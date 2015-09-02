f5636d03a9feb3b1ced7052c7cbc6d8ce25c8db7
albertosouza <alberto.souza.99@gmail.com>
2015-09-02 15:32:06 -0300

    update to v0.3.x

--- assets/README.md
+++
@@ -1,3 +0,0 @@
-# Theme assets folder
-
-Use to make your theme assets files like fonts and images and will be avaible as ***/theme/assets/[yourAsset]***
---
+++ theme.js
@@ -0,0 +1,44 @@
+/**
+ * @module  Theme
+ * @name    we-theme-blog-startbootstrap-clean
+ */
+
+module.exports = {
+  // theme config
+  configs: {
+    emailTemplates: {
+      path: 'templates/email',
+    },
+    javascript: 'files/public/js/main.js',
+    stylesheet: 'files/public/css/main.css'
+  },
+  autoLoadAllTemplates: true,
+  // will be auto loaded
+  templates: {},
+  // set layouts here
+  layouts: {
+    'default': {
+      template: __dirname + '/templates/server/layouts/full-layout.hbs',
+      regions: {
+        highlighted: {
+          name: 'Highlighted'
+        },
+        afterContent: {
+          name: 'After Content'
+        }
+      }
+    },
+    'article/layout': {
+      template: __dirname + '/templates/server/layouts/article.hbs',
+      regions: {
+        highlighted: {
+          name: 'Highlighted'
+        },
+        afterContent: {
+          name: 'After Content'
+        }
+      }
+    }
+  },
+  widgets: {}
+};
